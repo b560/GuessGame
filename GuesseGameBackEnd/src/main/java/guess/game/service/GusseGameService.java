5d283e96d07fd75bb71e2bccad73c452bb843345
Taofeeq Rasaq <babstuney32@gmail.com>
2024-08-19 19:57:54 -0400

    add a invalid input exception scebario for the backend

--- GuesseGameBackEnd/src/main/java/guess/game/service/GusseGameService.java
+++ GuesseGameBackEnd/src/main/java/guess/game/service/GusseGameService.java
@@ -6,45 +6,46 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-
-
 @Component
 public class GusseGameService {
-	
+
 	@Value("${original.word}")
-	private String originalWord ;
-	
-	@Value("#{'${guess.description}'.split(',')}") 
+	private String originalWord;
+
+	@Value("#{'${guess.description}'.split(',')}")
 	private List<String> guessDescriptionList;
-	
+
 	private final static String LETTER = "Letter";
-	
 	private final static String EMPTY = " ";
-	
 	private final static String COMMA = ",";
-	
-	public List<String> validateCharacters(String guessedWord){
+
+	public List<String> validateCharacters(String guessedWord) {
+
+		if (guessedWord == null || guessedWord.length() != 5) {
+			throw new RuntimeException("The guessed string length must be equal to 5");
+		}
 		String characterDescription = "";
-		int correctGuesse =0;
+		int correctGuesse = 0;
 		StringBuilder sb = new StringBuilder();
-		for(int i =0;i < guessedWord.length(); i++) {
-			if(originalWord.contains(String.valueOf(guessedWord.charAt(i)))) {
-				if(guessedWord.charAt(i) == originalWord.charAt(i)) {
+		for (int i = 0; i < guessedWord.length(); i++) {
+			if (originalWord.contains(String.valueOf(guessedWord.charAt(i)))) {
+				if (guessedWord.charAt(i) == originalWord.charAt(i)) {
 					characterDescription = guessDescriptionList.get(0);
 					correctGuesse++;
-				}else {
+				} else {
 					characterDescription = guessDescriptionList.get(1);
 				}
-			}else {
+			} else {
 				characterDescription = guessDescriptionList.get(2);
 			}
-			
-			    sb.append(LETTER).append(EMPTY).append(guessedWord.charAt(i)).append(EMPTY).append(characterDescription).append(COMMA);
+
+			sb.append(LETTER).append(EMPTY).append(guessedWord.charAt(i)).append(EMPTY).append(characterDescription)
+					.append(COMMA);
 		}
-		 sb.append(correctGuesse);
-		
-		 return Arrays.asList(sb.toString().split(COMMA));
-		 
+		sb.append(correctGuesse);
+
+		return Arrays.asList(sb.toString().split(COMMA));
+
 	}
 
 }
--- GuesseGameBackEnd/src/test/java/guess/game/service/GuessGameServiceTest.java
+++ GuesseGameBackEnd/src/test/java/guess/game/service/GuessGameServiceTest.java
@@ -13,30 +13,51 @@ import guess.game.service.GusseGameService;
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
 public class GuessGameServiceTest {
-	
+
 	@Autowired
 	private GusseGameService guessGameService;
-	
+
 	@Test
 	public void validate_word_with_incorrect_characters() {
 		List<String> results = guessGameService.validateCharacters("hello");
- 		Assertions.assertEquals(results.get(0), "Letter h is not found in the word");
+		Assertions.assertEquals(results.get(0), "Letter h is not found in the word");
 		Assertions.assertEquals(results.get(1), "Letter e is not found in the word");
 		Assertions.assertTrue(results.contains("Letter e is not found in the word"));
-		Assertions.assertEquals(results.get(4), "Letter o is found in the word BUT it is incorrectly positioned");	
-		//assert the total total of guess that are right
-		Assertions.assertEquals(results.get(5), "1");	
+		Assertions.assertEquals(results.get(4), "Letter o is found in the word BUT it is incorrectly positioned");
+		// assert the total total of guess that are right
+		Assertions.assertEquals(results.get(5), "1");
 	}
-	
+
 	@Test
 	public void validate_word_with_complete_correct_characters() {
 		List<String> results = guessGameService.validateCharacters("world");
- 		Assertions.assertEquals(results.get(0), "Letter w is found in the word AND it is in the correct position");
+		Assertions.assertEquals(results.get(0), "Letter w is found in the word AND it is in the correct position");
 		Assertions.assertEquals(results.get(1), "Letter o is found in the word AND it is in the correct position");
 		Assertions.assertTrue(results.contains("Letter r is found in the word AND it is in the correct position"));
-		Assertions.assertEquals(results.get(4), "Letter d is found in the word AND it is in the correct position");	
-		//assert the total total of guess that are right
-		Assertions.assertEquals(results.get(5), "5");	
+		Assertions.assertEquals(results.get(4), "Letter d is found in the word AND it is in the correct position");
+		// assert the total total of guess that are right
+		Assertions.assertEquals(results.get(5), "5");
+	}
+
+	@Test
+	public void null_guesse_input_should_return_exception() throws Exception {
+		Throwable exception = Assertions.assertThrows(RuntimeException.class,
+				() -> guessGameService.validateCharacters(null));
+		Assertions.assertEquals("The guessed string length must be equal to 5", exception.getMessage());
+	}
+
+	@Test
+	public void guesse_input_greaterthan_five_should_return_exception() throws Exception {
+		Throwable exception = Assertions.assertThrows(RuntimeException.class,
+				() -> guessGameService.validateCharacters("Friends"));
+		Assertions.assertEquals("The guessed string length must be equal to 5", exception.getMessage());
+	}
+	
+	@Test
+	public void guesse_input_lessthan_five_should_return_exception() throws Exception {
+		Throwable exception = Assertions.assertThrows(RuntimeException.class,
+				() -> guessGameService.validateCharacters("Lies"));
+		Assertions.assertEquals("The guessed string length must be equal to 5", exception.getMessage());
 	}
 
 }
